4ad93c4e5de21b736c2011bddcced1dab7f26091
YehiaM23 <yeehiamoataz798@gmail.com>
2025-04-24 17:18:57 +0200

    This is the js code for  the interface

---
+++ js/interface.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const searchButton = document.querySelector('.search-box button');
+
+  searchButton.addEventListener('click', function() {
+    alert('Search functionality is not yet implemented.');
+  });
+});
